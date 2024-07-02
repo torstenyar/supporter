11ea7deb0e5419c80c7611305c74a41faefad8e3
torstenyar <torsten@yarado.com>
2024-07-02 10:27:39 +0200

    added logging capabilities

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
 from flask import Flask, request, jsonify
 from slack_integration.event_handler import handle_event
 import logging
+# Import the `configure_azure_monitor()` function from the `azure.monitor.opentelemetry` package.
+from azure.monitor.opentelemetry import configure_azure_monitor
+import os
+# Configure OpenTelemetry to use Azure Monitor with the specified connection string.
+
+# Read the connection string from the environment variable
+connection_string = os.getenv('AZURE_LOG_CONNECTION_STRING')
+
+configure_azure_monitor(
+    connection_string=connection_string,
+)
 
 app = Flask(__name__)
 
