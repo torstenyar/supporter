f7984141a917e7accdfacb602c14a606566f45f1
torstenyar <torsten@yarado.com>
2024-08-07 18:22:03 +0200

    Fixed issue

--- slack_integration/event_handler.py
+++ slack_integration/event_handler.py
@@ -5,13 +5,11 @@ from azure.servicebus import ServiceBusClient, ServiceBusMessage
 from .message_handler import fetch_message, send_message
 from .slack_client import bot_user_id
 from utils.azure_openai_client import initialize_client
-from utils.supporter import load_log_file
-from utils.supporter import load_screenshot
-from utils.supporter import determine_point_of_failure
-from utils.supporter import load_log_preceding_steps
-from utils.supporter import generate_error_description
-from utils.supporter import perform_cause_analysis
-from utils.supporter import extract_data_from_message
+from utils.supporter import (
+    load_log_file, load_screenshot, determine_point_of_failure,
+    load_log_preceding_steps, generate_error_description,
+    perform_cause_analysis, extract_data_from_message
+)
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
@@ -42,9 +40,9 @@ def send_supporter_data_to_uardi(data):
         logging.error(f"Failed to send message to queue: {e}")
 
 def handle_event(data):
-    global reaction_tracker  # Ensure we are using the global reaction_tracker
+    global reaction_tracker
     event = data.get('event', {})
-    logging.info("Received event: {}".format(event))
+    logging.info(f"Received event: {event}")
     az_client = initialize_client()
 
     valid_reactions = ['yara-sup-1', 'yara-sup-backup']
@@ -60,36 +58,28 @@ def handle_event(data):
         message_timestamp = event['item']['ts']
         event_timestamp = event['event_ts']
 
-        # Check if the channel is allowed
         if channel_id not in ALLOWED_CHANNELS:
             logging.info("Reaction added in a non-allowed channel. Ignoring the event.")
             return
 
-        logging.info(
-            "Handling reaction_added event for channel {} and timestamp {} and event_timestamp {}".format(channel_id,
-                                                                                                          message_timestamp,
-                                                                                                          event_timestamp))
+        logging.info(f"Handling reaction_added event for channel {channel_id} and timestamp {message_timestamp} and event_timestamp {event_timestamp}")
 
-        # Check if this reaction was already processed for the same user and emoji
         if reaction_tracker.get((channel_id, message_timestamp, user_id, reaction)):
             logging.info("This reaction has already been processed for this user and emoji.")
             return
 
-        # Mark this reaction as processed for this user and emoji
         reaction_tracker[(channel_id, message_timestamp, user_id, reaction)] = True
 
         message = fetch_message(channel_id, message_timestamp)
         if message:
             logging.info("Fetched message!")
 
-            # Extract data from the message
             client_name, task_name, prio, run_id = extract_data_from_message(message)
-            logging.info("Client Name: {}".format(client_name))
-            logging.info("Task Name: {}".format(task_name))
-            logging.info("Prio: {}".format(prio))
-            logging.info("Run ID: {}".format(run_id))
+            logging.info(f"Client Name: {client_name}")
+            logging.info(f"Task Name: {task_name}")
+            logging.info(f"Prio: {prio}")
+            logging.info(f"Run ID: {run_id}")
 
-            # Check if any element is None and send a failure message if so
             if not all([client_name, task_name, run_id]):
                 error_message = ":warning: Error: I was unable to extract the necessary information from the message :cry:.\n"
                 if not client_name:
@@ -104,7 +94,6 @@ def handle_event(data):
                 return
 
             if '.yrd' in task_name:
-                logging.info("This error originates from a user/manually triggered run.")
                 error_message = (
                     ":confused: Warning: It looks like this error originates from a user/manual triggered run. "
                     "Currently, I am unable to handle these types of error and am solely focused on cloud orchestrated runs. "
@@ -113,12 +102,10 @@ def handle_event(data):
                 logging.error(error_message)
                 return
 
-            # Send initial acknowledgment message
             initial_message = ("Thanks for your request! I will take a moment to analyze the cause of this error. Will "
                                "come back to you ASAP :hourglass_flowing_sand:")
             send_message(channel_id, message_timestamp, initial_message, as_text=True)
 
-            # Load log data and screenshot
             log_file = load_log_file(run_id)
             if log_file is None:
                 error_message = ":warning: Error: Unable to fetch the log file. Please try again later."
@@ -139,17 +126,13 @@ def handle_event(data):
                 send_message(channel_id, message_timestamp, error_message, as_text=True)
                 return
 
-            # Determine point of failure
             point_of_failure_descr, failed_step_id = determine_point_of_failure(log_file)
-
-            # Load the preceding steps
             preceding_steps_log = load_log_preceding_steps(log_file, failed_step_id, steps_to_include=10)
 
             retry_count = 0
             max_retries = 3
             while retry_count < max_retries:
                 try:
-                    # Generate error description and cause analysis
                     error_description = generate_error_description(az_client, client_name, task_name,
                                                                    point_of_failure_descr,
                                                                    preceding_steps_log, screenshot)
@@ -157,7 +140,6 @@ def handle_event(data):
                                                             screenshot,
                                                             error_description)
 
-                    # Create the response blocks
                     blocks_analysis = json.dumps([
                         {
                             "type": "section",
@@ -177,25 +159,26 @@ def handle_event(data):
                         json.loads(cause_analysis)
                     ])
 
-                    # Send the detailed response message using blocks
                     send_message(channel_id, message_timestamp, blocks_analysis, as_text=False)
 
-                    # Send run id with supporter tag to the Service Bus queue
-                    send_supporter_data_to_uardi({
+                    supporter_data = {
                         "task_run_id": run_id,
                         "task_name": task_name,
-                        "client_name": client_name,
-                        "point_of_failure_descr": point_of_failure_descr,
-                        "preceding_steps_log": preceding_steps_log
-                    })
+                        "organisation_name": client_name,
+                        "step_id_pof": failed_step_id,
+                        "ai_cause": json.loads(cause_analysis)['text']['text'],
+                        "ai_description": json.loads(error_description)['text']['text']
+                    }
+                    send_supporter_data_to_uardi(supporter_data)
 
-                    break  # Exit loop if message is sent successfully
+                    break
 
                 except Exception as e:
-                    logging.error("Failed to send message: {}. Retry {}/{}".format(e, retry_count + 1, max_retries))
+                    logging.error(f"Failed to send message: {e}. Retry {retry_count + 1}/{max_retries}")
                     retry_count += 1
                     if retry_count == max_retries:
-                        error_message = ":warning: Error: Unable to send the analysis message after multiple attempts. Please try again later."
+                        error_message = (":warning: Error: Unable to send the analysis message after multiple "
+                                         "attempts. Please try again later.")
                         send_message(channel_id, message_timestamp, error_message, as_text=True)
                         return
 
@@ -209,10 +192,7 @@ def handle_event(data):
         channel_id = event['item']['channel']
         message_timestamp = event['item']['ts']
 
-        logging.info(
-            "Handling reaction_removed event for channel {} and timestamp {}".format(channel_id, message_timestamp))
+        logging.info(f"Handling reaction_removed event for channel {channel_id} and timestamp {message_timestamp}")
 
-        # Remove the processed mark
         if reaction_tracker.get((channel_id, message_timestamp, user_id, reaction)):
             del reaction_tracker[(channel_id, message_timestamp, user_id, reaction)]
-
